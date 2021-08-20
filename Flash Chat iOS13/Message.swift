285d812b340009dffc961c8f3ca8c1eeab11c053
qNecro <bafordsk@gmail.com>
2021-08-20 16:46:28 +0200

    .

---
+++ Flash Chat iOS13/Message.swift
@@ -0,0 +1,15 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Michal on 19/08/2021.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+    
+}
